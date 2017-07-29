3b420eadd9ac5e1fa7af30ebe2c4bfb70d1e4c71
yuliaua1 <yuliaua@yahoo.com>
2017-07-28 19:44:06 -0700

    Added list comprehension method to create the list.

--- RandomList_01.py
+++ RandomList_01.py
@@ -7,3 +7,8 @@ randomList = []
 for i in range(20):
     randomList.append(random.randint(1, 50))
 print(randomList)
+
+#This is creating the list using list comprehensions
+randomList = [ random.randint(1, 50) for i in range(20) ]
+
+print(randomList)
