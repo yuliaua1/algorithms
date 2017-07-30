63054cb875d986be92e2b6b1bf690a52e35488c8
yuliaua1 <yuliaua@yahoo.com>
2017-07-29 20:17:49 -0700

    Added code to authenticate to Twitter.

--- Twitterapp.py
+++ Twitterapp.py
@@ -1,10 +1,23 @@
 # this program is to connect to Twitter resources.
 
 import twitter
+from os.path import expanduser
+
+home = expanduser("~")
+l = []
+x = home + '/Documents/Development/Y.txt'
+
+with open( x, 'r') as f:
+    l = f.readlines()
+
+tapi = twitter.Api(consumer_key=l[0].strip(),
+                      consumer_secret=l[1].strip(),
+                      access_token_key=l[2].strip(),
+                      access_token_secret=l[3].strip())
 
 
-taccountholderFriends = taccountholderapi.GetFriends()
+taccountholderFriends = tapi.GetFriends()
 print (taccountholderFriends)
 
-status = taccountholderapi.PostUpdate('place holder')
-print(status.text)
+#status = taccountholderapi.PostUpdate('place holder')
+#print(status.text)
