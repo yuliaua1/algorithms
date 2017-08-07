0327ca3c0bab9384d7797ebcc4dad11fa2d87e88
yuliaua1 <yuliaua@yahoo.com>
2017-08-07 16:21:25 -0700

    updated file with the pprint function for the readability of the code

--- hashtag_search.py
+++ hashtag_search.py
@@ -1,14 +1,9 @@
 
-# Write a new program in a new file to search for the following hashtags:
-# Apollo11
-# Fashion
-# Print the type of search result
-# Based on the type of the search result (list or dict), iterate through the results and print them in a human readable form.
-# Checkin the new program into Github.
 
-
+import pprint
 from twitter import Twitter, OAuth
 from os.path import expanduser
+
 import pprint
 home = expanduser("~")
 l = []
@@ -22,21 +17,25 @@ myoauth = OAuth (l[2].strip(), l[3].strip(), l[0].strip(), l[1].strip())
 
 myT = Twitter(auth = myoauth)
 X = myT.search.tweets(q = "#Apollo11")
-Y = myT.search.tweets(q = "#Fassion")
 
 print('-' *40)
 
 print(type(X))
-print(type(Y))
 
 print('-' *40)
 
 
 for key, value in X.iteritems():
-    print(key, value)
+    print('\t' + str(key))
+    #print(type(value))
 print('-'*40)
 
+print("just for the sake of my happiness {}\n", type(X['statuses']))
+for e in X['statuses']:
+	pprint.pprint( e )
+	print(50* '=')
 
-for key, value in Y.iteritems():
-    print(key, value)
-print('-'*40)
+
+
+
+
