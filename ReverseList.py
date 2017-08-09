ab9e3ba3fb1749e0bbbfcbe74289193c1d360431
yuliaua1 <yuliaua@yahoo.com>
2017-08-09 13:28:41 -0700

    Modified list names to correspond with the nature of the content.Complied with the spec.

--- ReverseList.py
+++ ReverseList.py
@@ -6,20 +6,21 @@ import random
 originalList =[]
 for i in range(10):
 	originalList.append(random.randint(1,100))
-print(originalList)
+
 
 #--------------------------------------------------------
-
 def revfunction():
-	reverseList =[]
+	tempRevList =[]
 	for i in range(len(originalList)):
-		reverseList.append(originalList[len(originalList) - i -1])
+		tempRevList.append(originalList[len(originalList) - i -1])
 
-	return(reverseList)
-#---------------------------------------------------------
+	return(tempRevList)
+#----------------------------------------------------------
+
+reverseList = revfunction()
+
+print('Please see below the original list: ')
+print(originalList)
 print('Please see below the reverse list: ')
-
-ModifList = revfunction()
-
-print(ModifList)
+print(reverseList)
 
