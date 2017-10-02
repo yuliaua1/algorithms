6ce623ed453788766a31b7fe0d5e9915534f82a6
yuliaua1 <yuliaua@yahoo.com>
2017-10-02 13:37:00 -0700

    Checking in new code with Kazem.

---
+++ masterMindX/masterMindX/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  masterMindX
+//
+//  Created by Yuliya  Raldugina on 10/2/17.
+//  Copyright © 2017 Yuliya  Raldugina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        print("New game")
+    }
+    
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+}
+
